21a901c0094c2c45a9ea2b47b14456243aa1c268
Farok Hossain <farokafs7@gmail.com>
2024-08-19 23:46:18 +0600

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <h1 className=" text-4xl text-red-700 text-center">Hello E-Zilly</h1>
+      <button className="btn btn-active btn-accent">Accent</button>
     </div>
   );
 };
