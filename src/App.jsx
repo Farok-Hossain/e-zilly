6e806e9554406b42966803eb3fda2cc5174ab082
Farok Hossain <farokafs7@gmail.com>
2024-08-19 23:44:40 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className=" text-4xl text-red-700">Hello E-Zilly</h1>
+      <h1 className=" text-4xl text-red-700 text-center">Hello E-Zilly</h1>
     </div>
   );
 };
