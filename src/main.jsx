111ad4a8bc58bb7ff2e136a869bd77b1c0a02eed
Farok Hossain <farokafs7@gmail.com>
2024-08-21 21:24:34 +0600

    Navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
+import Navbar from "./components/Pages/Shared/Navbar/Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar></Navbar>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
