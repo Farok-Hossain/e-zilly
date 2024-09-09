6947b0a0459d8c5c65d79e6b6751347a5ebec223
Farok Hossain <farokafs7@gmail.com>
2024-09-09 20:33:31 +0600

    Instagram icons added

--- src/components/Pages/Home/Home/Home.jsx
+++ src/components/Pages/Home/Home/Home.jsx
@@ -1,12 +1,20 @@
 import { Helmet } from "react-helmet";
+import { FaInstagram } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <Helmet>
         <title>EZilly | Home</title>
       </Helmet>
       <h1>This is home</h1>
+      <h1>This is home</h1>
+      <h1>This is home</h1>
+      <h1>This is home</h1>
+      <Link to="https://www.instagram.com/farok_afs/" target="_blank">
+        <FaInstagram />
+      </Link>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import Navbar from "./components/Pages/Shared/Navbar/Navbar.jsx";
+import Footer from "./components/Pages/Shared/Footer/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar></Navbar>
     <RouterProvider router={router} />
+    <Footer></Footer>
   </React.StrictMode>
 );
