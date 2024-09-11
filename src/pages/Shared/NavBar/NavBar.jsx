ec5b8c4b6d06ec0d472ef1cf7710af7bd549ca97
Farok Hossain <farokafs7@gmail.com>
2024-09-11 22:53:59 +0600

    NavBar updated

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -26,22 +26,13 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
+            <Link to="/home">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/login">login</Link>
             <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <Link to="https://www.instagram.com/farok_afs/" target="_blank">
+                <FaInstagram />
+              </Link>
             </li>
           </ul>
         </div>
